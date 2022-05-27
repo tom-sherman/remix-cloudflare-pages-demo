c4294bd1e640f711b6c94b6d9c7fea5f8b81059b
Tom Sherman <the.tomsherman@gmail.com>
2022-05-27 09:31:34 +0100

    Display current timestamp via loader

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,16 @@
+import { json, LoaderFunction } from "@remix-run/cloudflare";
+import { useLoaderData } from "@remix-run/react";
+
+export const loader: LoaderFunction = () => json({
+  now: Date.now()
+})
+
 export default function Index() {
+  const data = useLoaderData();
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
+      <h1>Welcome to Remix {data.now}</h1>
       <ul>
         <li>
           <a
